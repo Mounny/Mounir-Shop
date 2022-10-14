a1aab4c4a5ceb237d6f487aaa6db926b885d2a81
Mounny <benmounny@gmail.com>
2022-10-14 08:33:29 +0100

    first commit

---
+++ frontend/src/products.js
@@ -0,0 +1,93 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Apple TV',
+        image: '/images/appletv.jpg',
+        description:
+        'The best Apple TV lets you watch the latest movies and shows. It is a great product for you and your family',
+        brand: ' Apple',
+        category:'Electronics',
+        price: 79.50,
+        countInStock: 7,
+        rating: 4,
+        numReviews: 5,
+    },
+
+    {
+        _id: '2',
+        name: 'Fire TV',
+        image: '/images/firetv.jpg',
+        description:
+        'The best Amazon Fire TVlets you watch the latest movies and shows. It is a great product for you and your family',
+        brand: ' Amazon',
+        category:'Electronics',
+        price: 49.50,
+        countInStock: 5,
+        rating: 3,
+        numReviews: 4,
+    },
+
+    {
+        _id: '3',
+        name: 'Headset',
+        image: '/images/headset.jpg',
+        description:
+        'The best Headset lets you listen to the latest shows. It is a great product for you ',
+        brand: ' Apple',
+        category:'Electronics',
+        price: 19.50,
+        countInStock: 23,
+        rating: 4,
+        numReviews: 3,
+    },
+
+    {
+        _id: '4',
+        name: 'iwatch',
+        image: '/images/iwatch.jpg',
+        description:
+        'The latest iWatch in the market with the newest features ',
+        brand: ' Apple',
+        category:'Electronics',
+        price: 279.50,
+        countInStock: 14,
+        rating: 4.5,
+        numReviews: 4,
+    },
+
+    {
+        _id: '5',
+        name: 'keyboard',
+        image: '/images/keyboard.jpg',
+        description:
+        'The latest keyboard in the market with the best features ',
+        brand: ' logitech',
+        category:'Electronics',
+        price: 39.50,
+        countInStock: 11,
+        rating: 4,
+        numReviews: 4,
+    },
+
+    {
+        _id: '6',
+        name: 'laptop',
+        image: '/images/laptop.jpg',
+        description:
+        'The latest laptop in the market with the best features ',
+        brand: 'hp',
+        category:'Electronics',
+        price: 399.50,
+        countInStock: 10,
+        rating: 5,
+        numReviews: 4,
+    },
+
+
+
+
+
+
+]
+
+export default products
